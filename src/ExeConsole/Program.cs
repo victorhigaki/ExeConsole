f189433e6e1434122c89d81209047d75d230ec22
Victor Higaki <victorhigaki@outlook.com>
2024-02-13 22:56:59 -0300

    feat: add HostApplicationBuilder

--- src/ExeConsole/Program.cs
+++ src/ExeConsole/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
